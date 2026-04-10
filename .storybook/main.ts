6bc24b9f23d5ec8967c39865461669fa65ffeabd
yuunhae <susu492@naver.com>
2026-04-10 16:45:57 +0900

    feat : update story book

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,37 +1,31 @@
-import path from 'path';
-import type { StorybookConfig } from '@storybook/react-vite';
-import svgr from 'vite-plugin-svgr';
+import path from 'path'
+import type { StorybookConfig } from '@storybook/react-vite'
+import svgr from 'vite-plugin-svgr'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
     '@storybook/addon-actions',
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test",
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
-  viteFinal: async (config) => {
-    config.resolve = config.resolve || {};
+  viteFinal: async config => {
+    config.resolve = config.resolve || {}
     config.resolve.alias = {
-      ...(config.resolve.alias as Record<string,string>),
+      ...(config.resolve.alias as Record<string, string>),
       '@': path.resolve(__dirname, '../src'),
-    };
+    }
 
-    config.plugins = [...(config.plugins || []), svgr()];
+    config.plugins = [...(config.plugins || [])]
 
-    return config;
+    return config
   },
+}
 
-  }
-
-export default config;
-
-
+export default config
--- src/lib/components/calendar/Calendar.stories.tsx
+++ src/lib/components/calendar/Calendar.stories.tsx
@@ -1,24 +1,66 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react'
 
-import Calendar from './Calendar';
+import Calendar from './Calendar'
 const meta: Meta<typeof Calendar> = {
   title: 'Calendar',
   component: Calendar,
-};
+}
 
-export default meta;
-type Story = StoryObj<typeof Calendar>;
+export default meta
+type Story = StoryObj<typeof Calendar>
 
 export const Default: Story = {
-  render: () => <Calendar size="sm" />,
-};
+  render: () => (
+    <Calendar
+      style="w-550"
+      mockdata={[
+        {
+          id: '1',
+          date: '2026-04-09',
+          label: 'meeting',
+          color: 'primary',
+        },
+        {
+          id: '2',
+          date: '2026-04-10',
+          label: 'holiday',
+          color: 'green',
+        },
+        {
+          id: '4',
+          date: '2026-04-25',
+          label: 'pilates',
+          color: 'blue',
+        },
+      ]}
+    />
+  ),
+}
 
-export const sm: Story = {
-  render: () => <Calendar size="sm" />,
-};
-export const md: Story = {
-  render: () => <Calendar size="md" />,
-};
 export const lg: Story = {
-  render: () => <Calendar size="lg" />,
-};
+  render: () => (
+    <Calendar
+      style="w-700"
+      mockdata={[
+        {
+          id: '1',
+          date: '2026-04-09',
+          label: 'meeting',
+          color: 'primary',
+        },
+        {
+          id: '2',
+          date: '2026-04-10',
+          label: 'holiday',
+          color: 'green',
+        },
+        {
+          id: '4',
+          date: '2026-04-25',
+          label: 'pilates',
+          color: 'blue',
+        },
+      ]}
+    />
+  ),
+}
